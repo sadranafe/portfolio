33be047cf47c654990f99c9b065a1cc7e845a948
mohammad sadra nafe <sadranafe7@gmail.com>
2026-06-18 20:36:39 +0330

    chore : move the UI folder to the components folder

--- src/app/components/layout/HeroSection.js
+++ src/app/components/layout/HeroSection.js
@@ -10,13 +10,13 @@ const HeroSection = () => {
                 </div>
 
                 <div>
-                    <h1 className = "hero-title font-display font-extrabold text-8xl">sadra <br/> nafe</h1>
+                    <h1 className = "hero-title drop-shadow-[0_0_90px_rgba(34,211,238,1)] dark:drop-shadow-[0_0_90px_rgba(34,211,238,0.5)] font-display font-extrabold text-8xl">sadra <br/> nafe</h1>
                 </div>
                 
                 <div className = "w-20 h-0.5 rounded-full bg-sky-500"></div>
 
                 <div className = "mt-2 flex max-[300px]:flex-wrap justify-center items-center gap-4">
-                    <Link href = '#work' className = "capitalize rounded-lg py-2.5 px-3 bg-sky-500 dark:bg-sky-600 text-white">view work</Link>
+                    <Link href = '#work' className = "capitalize rounded-lg py-2.5 px-3 bg-sky-600 text-white">view work</Link>
                     <Link href = '#contact' className = "border capitalize border-neutral-200 dark:border-neutral-700 dark:hover:bg-neutral-700 dark:hover:border-neutral-700 hover:bg-neutral-200/50 transition-all p-2.5 px-3 rounded-lg text-sky-600">Get in touch</Link>
                 </div>
             </div>
